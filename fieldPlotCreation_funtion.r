28afb640a36499ce92695aa63e3312123483a9c2
Dave-Evans <evans.dave.michael@gmail.com>
2015-11-19 19:47:43 -0600

    Made revisions, could still use testing

--- fieldPlotCreation_funtion.r
+++ fieldPlotCreation_funtion.r
@@ -5,20 +5,21 @@ library(rgeos)
 # library(maptools)
 # use only feet or meters
 # first four inputs should be geopraphic coordinates in wgs84, straight from GoogleMaps/Earth
-llLat = 43.299066      # lower left latitude  
-llLong = -89.354851     # lower left longitude
-ulLat = 43.299226    # upper left corner latitude
-ulLong = -89.354847   # upper left corner longitude
-lngth = 30             # length of plot
-wdth = 5              # width of plot
-NtoS = 2               # number of plots in the rep running north to south
-EtoW = 5               # no of plots in the rep running east to west
-fileName = "A2sp_Rep4_v2" # name of file --- currently not implemented
-metric = F             # are the length and width in metric or english?
+#llLat = 43.299066      # lower left latitude  
+#llLong = -89.354851     # lower left longitude
+#ulLat = 43.299226    # upper left corner latitude
+#ulLong = -89.354847   # upper left corner longitude
+#lngth = 30             # length of plot
+#wdth = 5              # width of plot
+#NtoS = 2               # number of plots in the rep running north to south
+#EtoW = 5               # no of plots in the rep running east to west
+#fileName = "A2sp_Rep4_v2" # name of file --- currently not implemented
+#metric = F             # are the length and width in metric or english?
 
-plotRep <- function(llLat, llLong, ulLat, ulLong, lngth, wdth, NtoS, EtoW, 
+plotRep <- function(positionVect, lngth, wdth, NtoS, EtoW, #llLat, llLong, ulLat, ulLong
                     fileName = paste('plot_',as.double(Sys.time()), sep = ''), metric = T){
-    corners <- matrix(c(llLat, llLong, ulLat, ulLong), 2,2, T)
+#    corners <- matrix(c(llLat, llLong, ulLat, ulLong), 2,2, T)
+	corners <- matrix(positionVect, 2, 2, T)
     rownames(corners) <- c('ll','ul')
     colnames(corners) <- c('lat','long')
     corners <- data.frame(corners)
@@ -31,17 +32,18 @@ plotRep <- function(llLat, llLong, ulLat, ulLong, lngth, wdth, NtoS, EtoW,
        lngth <- lngth * 0.3048
        wdth <- wdth * 0.3048
     }
+	### Maybe incorporate a try catch here to force the proper dimension
     y_seq <- seq(crds[1,2],
-              crds[2,2]+(lngth/2), lngth)
+              crds[2,2]+lngth/2, length.out=NtoS+1)#lngth)
     x_seq <- seq(crds[1,1],
-              crds[1,1]+(EtoW*wdth), wdth)
+              crds[1,1]+(EtoW*wdth), length.out=EtoW)#wdth)
 	cnter = 0
 	polyStrList = "MULTIPOLYGON("
 	
 	for (y in y_seq){
 		if (y == max(y_seq)) { break }
 		for (x in x_seq){
-			if (x == max(x_seq)) { break }
+#			if (x == max(x_seq)) { break }
 			cnter=cnter+1
 			pt1 = paste(x,y)
 			pt2 = paste(x,y+lngth)
@@ -62,7 +64,8 @@ plotRep <- function(llLat, llLong, ulLat, ulLong, lngth, wdth, NtoS, EtoW,
 	spPolys = disaggregate(spPolys)
     spdf <- SpatialPolygonsDataFrame(spPolys,dat)
     proj4string(spdf) <- wtm
-    #plot(spdf)
+    plot(spdf)
+	text(getSpPPolygonsLabptSlots(spdf), labels=spdf@data$id)
     
 #    labls <- NULL
 #    for (lbl in 1:length(spdf@polygons)){
@@ -76,6 +79,6 @@ plotRep <- function(llLat, llLong, ulLat, ulLong, lngth, wdth, NtoS, EtoW,
     #Potentially  future support for kml or shapefile export
 #     writeOGR(spdfExp['id'], paste(fileName,'.kml',sep = ''), 'id',driver = 'KML')
     # writing a shapefile
-#     writeOGR(spdf, paste(fileName), 'id',driver = 'ESRI Shapefile')
+     writeOGR(spdf,"test.shp", 'id',driver = 'ESRI Shapefile')
 }
 
